3b98c40087e4b9359ba54e672338ee0c33008413
Ulugbek Abdullaev <ulugbekna@gmail.com>
2023-02-20 21:05:39 +0100

    context keys: parser: support for running without regex parsing error recovery

--- src/vs/platform/contextkey/common/contextkey.ts
+++ src/vs/platform/contextkey/common/contextkey.ts
@@ -292,6 +292,14 @@ value ::=
 ```
 */
 
+export type ParserConfig = {
+	regexParsingWithErrorRecovery: boolean;
+};
+
+const defaultConfig: ParserConfig = {
+	regexParsingWithErrorRecovery: true // with this option enabled, the parser can recover from regex parsing errors, e.g., unescaped slashes
+};
+
 class ParseError extends Error { }
 
 /**
@@ -329,6 +337,9 @@ export class Parser {
 
 	get parsingErrors(): Readonly<string[]> {
 		return this._parsingErrors;
+	}
+
+	constructor(private _config: ParserConfig = defaultConfig) {
 	}
 
 	/**
@@ -440,6 +451,19 @@ export class Parser {
 
 					// @ulugbekna: we need to reconstruct the regex from the tokens because some extensions use unescaped slashes in regexes
 					const expr = this._peek();
+
+					if (!this._config.regexParsingWithErrorRecovery) {
+						this._advance();
+						if (expr.type !== TokenType.RegexStr) {
+							throw this._errExpectedButGot(`REGEX`, expr);
+						}
+						const regexLexeme = expr.lexeme;
+						const closingSlashIndex = regexLexeme.lastIndexOf('/');
+						const flags = closingSlashIndex === regexLexeme.length - 1 ? undefined : regexLexeme.substring(closingSlashIndex + 1);
+						const regexp = new RegExp(regexLexeme.substring(1, closingSlashIndex), flags);
+						return ContextKeyExpr.regex(key, regexp);
+					}
+
 					switch (expr.type) {
 						case TokenType.RegexStr:
 						case TokenType.Error: { // also handle an ErrorToken in case of smth such as /(/file)/
